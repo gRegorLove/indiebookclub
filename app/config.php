fe390cb7c273c3878f2584459f9b977c7e717e19
Gregor Morrill <git@gregorlove.com>
2022-11-06 13:22:35 -0800

    Update dependencies and configuration

--- app/config.php
+++ app/config.php
@@ -5,24 +5,33 @@ declare(strict_types=1);
 use Dotenv\Dotenv;
 
 try {
-    // Load environment variables from .env file when not in production.
-    $dotenv = new Dotenv(dirname(__DIR__));
+    ## prepare environment
 
-    if (getenv('APP_ENV') !== 'production') {
+    # load cached environment only in production
+    $cached_env_file = __DIR__ . '/env.php';
+    if (file_exists($cached_env_file)) {
+        require $cached_env_file;
+    } else {
+        # otherwise load from .env file
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__));
         $dotenv->load();
         error_reporting(E_ALL);
         ini_set('display_errors', '1');
-    }
 
-    // Require these environment variables.
-    $dotenv->required([
-        'IBC_HOSTNAME',
-        'IBC_BASE_URL',
-        'IBC_DB_HOST',
-        'IBC_DB_NAME',
-        'IBC_DB_USERNAME',
-        'IBC_DB_PASSWORD',
-    ]);
+        ## require these environment variables
+        $dotenv->required([
+            'APP_ENV',
+            'IBC_EMAIL',
+            'IBC_HOSTNAME',
+            'IBC_BASE_URL',
+            'IBC_DB_HOST',
+            'IBC_DB_NAME',
+            'IBC_DB_USERNAME',
+            'IBC_DB_PASSWORD',
+            'LOG_DIR',
+            'LOG_NAME',
+        ]);
+    }
 } catch (RunTimeException $e) {
     echo $e->getMessage(); exit;
 }
@@ -38,7 +47,7 @@ if ($app_env !== 'production') {
 ini_set('session.name', $session_name);
 ini_set('session.auto_start', '0');
 ini_set('session.use_trans_sid', '0');
-ini_set('session.cookie_domain', getenv('IBC_HOSTNAME'));
+ini_set('session.cookie_domain', $_ENV['IBC_HOSTNAME']);
 ini_set('session.cookie_path', '/');
 ini_set('session.use_strict_mode', '1');
 ini_set('session.use_cookies', '1');
--- app/settings.php
+++ app/settings.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 return [
     'settings' => [
-        'version' => '0.0.3',
+        'version' => '0.1.0',
         'offline' => false,
         'developer_ip' => '127.0.0.1',
         'developer_domains' => [
             'gregorlove.com',
         ],
-        'displayErrorDetails' => (getenv('APP_ENV') !== 'production'),
+        'displayErrorDetails' => ($_ENV['APP_ENV'] !== 'production'),
         'determineRouteBeforeAppMiddleware' => true,
         'theme' => [
-            'theme_path' => __DIR__ . '/../themes/',
+            'public_path' => dirname(__DIR__) . '/public/',
+            'twig_path' => dirname(__DIR__) . '/templates/',
+            'twig_cache_path' => dirname(__DIR__) . '/cache/twig/',
+            'enable_twig_cache' => false,
         ]
     ],
 ];
