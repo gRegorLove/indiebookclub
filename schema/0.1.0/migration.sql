4677ee3641fdcbd34964a81194632c1bdb7aed44
Gregor Morrill <git@gregorlove.com>
2022-11-14 21:05:26 -0800

    Preparing next release
    Update the updates page
    Update migration.sql

--- schema/0.1.0/migration.sql
+++ schema/0.1.0/migration.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `entries` CHANGE COLUMN `canonical_url` varchar(255) NOT NULL DEFAULT '';
+ALTER TABLE `entries` CHANGE COLUMN `canonical_url` `canonical_url` VARCHAR(255) NOT NULL DEFAULT '';
 
-ALTER TABLE `users` ADD COLUMN `revocation_endpoint` varchar(255) NOT NULL DEFAULT '' AFTER `token_endpoint`;
+ALTER TABLE `users` ADD COLUMN `revocation_endpoint` VARCHAR(255) NOT NULL DEFAULT '' AFTER `token_endpoint`;
 
